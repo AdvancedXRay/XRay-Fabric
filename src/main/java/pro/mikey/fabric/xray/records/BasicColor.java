79a45ba6c5749bdbf9c9dd92170f44c637aa4194
Jens K <contact@jensk.dev>
2023-06-28 00:01:00 +0200

    add alpha channel support

--- src/main/java/pro/mikey/fabric/xray/records/BasicColor.java
+++ src/main/java/pro/mikey/fabric/xray/records/BasicColor.java
@@ -1,22 +1,29 @@
 package pro.mikey.fabric.xray.records;
 
-import java.awt.*;
-
 public record BasicColor(
         int red,
         int green,
-        int blue
+        int blue,
+        int alpha
 ) {
     public static BasicColor of(String hex) {
         if (!hex.startsWith("#")) {
-            return new BasicColor(0, 0, 0);
+            return new BasicColor(0, 0, 0, 255);
         }
 
-        Color color = Color.decode(hex);
-        return new BasicColor(color.getRed(), color.getGreen(), color.getBlue());
+        try {
+            final long color = Long.decode(hex);
+            final int alpha = (int)(color >> 24) & 0xFF;
+            final int red   = (int)(color >> 16) & 0xFF;
+            final int green = (int)(color >> 8)  & 0xFF;
+            final int blue  = (int)(color)       & 0xFF;
+            return new BasicColor(red, green, blue, alpha);
+        } catch (NumberFormatException e) {
+            return new BasicColor(0, 0, 0, 255);
+        }
     }
 
     String toHex() {
-        return String.format("#%02x%02x%02x", this.red, this.green, this.blue);
+        return String.format("#%02x%02x%02x%02x", this.alpha, this.red, this.green, this.blue);
     }
 }
